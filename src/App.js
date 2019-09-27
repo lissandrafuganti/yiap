c2f997009f3538f6499d237f96f28c7aeccae4a4
lissandrafuganti <lissandrafuganti@gmail.com>
2019-09-27 12:10:22 -0300

    css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Cena1 from './img/Cena1.png';
 import Cena2 from './img/Cena2.png';
 import Cena3 from './img/Cena3.png';
@@ -6,15 +6,40 @@ import Cena4 from './img/Cena4.png';
 import Logo from './img/logo.png';
 import Pontinhos from './img/pontinhos.png';
 
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {Navbar,Nav,Col,Row,Container} from 'react-bootstrap';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.handleScroll=this.handleScroll.bind(this);
+    this.state = { scrolling: false }
+  }
+ 
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+}
+
+componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+}
+handleScroll(event) {
+    if (window.scrollY === 0 && this.state.scrolling === true) {
+        this.setState({scrolling: false});
+    }
+    else if (window.scrollY !== 0 && this.state.scrolling !== true) {
+        this.setState({scrolling: true});
+    }
+}
+
+render() {
+  let teste = this.state.scrolling?'#d4c050 ':'transparent';
   return (
   
-   <Container>
-      <Navbar bg="light" expand="lg"  fixed="top" className="font1">
+    <Container> 
+  <Navbar  expand="lg"  fixed="top" className="font1" style={{background:teste}}>
   <Navbar.Brand href="#home"><img className="logo" src={Logo} alt=" imagem"/></Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
@@ -28,6 +53,7 @@ function App() {
       </Nav>
   </Navbar.Collapse>
 </Navbar>
+
 <br/><br/><br/><br/><br/><br/><br/><br/><br/>
 <Row className="cena1">
   <Col><h2 className="font2">“Primeiro sistema de força de vendas de verdade”
@@ -35,7 +61,7 @@ Automatizando processos e monitorando o mercado 24 horas por dia.
 </h2></Col><Col>
 <img className="cena1" src={Cena1} alt=" imagem"/></Col></Row>
 <br/><br/>
- <Row> <Col className="gradiente">
+ <Row> <Col className="gradiente" expand="lg">
   <br/><br/></Col></Row><br/><br/>
 <Row><Col>
 <img className="cena1" src={Cena2} alt="imagem"/> </Col><Col>
@@ -56,5 +82,5 @@ sistema monitor a hora de ressuprir.</h2></Col><Col>
     
   );
 }
-
+}
 export default App;
